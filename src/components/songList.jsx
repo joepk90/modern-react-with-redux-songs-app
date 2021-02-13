13b4bcf9bfe6c73de6bb200cb1565e9a7ff956aa
Joe <jparkkennaby@gmail.com>
2021-02-13 11:16:38 +0000

    245 - import selectSong action into SongList component. pass into the connect function when exporting the component + add documentation on the connect function interface. setup event hadnler on button within renderList method

--- src/components/songList.jsx
+++ src/components/songList.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions/index';
 
 class SongList extends Component {
 
@@ -9,7 +10,10 @@ class SongList extends Component {
             return (
                 <div className="item" key={song.title}>
                     <div className="right floated content">
-                        <button className="ui button primary">Select</button>
+                        <button
+                            className="ui button primary"
+                            onClick={() => this.props.selectSong(song)}
+                        >Select</button>
                     </div>
                     <div className="content">{song.title}</div>
                 </div>
@@ -32,4 +36,10 @@ const mapStateToProps = state => {
 
 }
 
-export default connect(mapStateToProps)(SongList);
+// The Connect() Function Explained
+// the first argument is a custom function defined above were we map redux state to this components props
+// the second argument is an object of dispatch functions (store.dispatch()) which also get's passed into our component as properties
+// the second argument is an object where we can pass our Actions (currently only selectSong is being passed in). These are used to update the state when an event is fired.
+// finally the SongsList component is passed as a callback function to the connect function
+// this means the SongsList component is rendered through a connect component where these properties get added to the components props
+export default connect(mapStateToProps, { selectSong })(SongList);
