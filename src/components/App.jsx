92f16af0d799d6d05eebd06701d73d6e7c7d56d4
Joe <jparkkennaby@gmail.com>
2021-02-13 12:52:16 +0000

    add title and subtitle descripting application

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,13 +5,20 @@ import SongDetail from './songDetail';
 
 const App = () => {
     return (
-        <div className="ui container grid">
-            <div className="ui row">
-                <div className="column eight wide">
-                    <SongList />
-                </div>
-                <div className="column eight wide">
-                    <SongDetail />
+        <div className="ui container">
+
+            <h1>Song List Application</h1>
+            <h3>Simple application demonstrating Redux (react-redux)</h3>
+
+            <div className="ui grid">
+                <div className="ui row">
+
+                    <div className="column eight wide">
+                        <SongList />
+                    </div>
+                    <div className="column eight wide">
+                        <SongDetail />
+                    </div>
                 </div>
             </div>
         </div>
